e820d4065e1c4d61580d4e47b44500e9b4309e0c
Hanjoo Kim <hanjoo0211@gmail.com>
2024-08-05 23:32:47 +0900

    Feat: Add select replyRoom

--- pynanabot/message/views.py
+++ pynanabot/message/views.py
@@ -1,7 +1,7 @@
-from .models import ReceivedMessage, SentMessage
 from rest_framework import permissions, viewsets
 from rest_framework.response import Response
 
+from .models import ReceivedMessage, SentMessage
 from .serializers import ReceivedMessageSerializer, SentMessageSerializer
 
 
@@ -33,6 +33,7 @@ class ReplyViewSet(viewsets.ViewSet):
         )
 
         reply_message = None
+        reply_room = room # 기본값은 메시지 받은 방으로 설정
 
         # 메시지 분기 필요
         if "시치" in message:
@@ -43,12 +44,16 @@ class ReplyViewSet(viewsets.ViewSet):
                     if not prior_sic:
                         prior_sic = "페리"
                     reply_message = f"아오 {prior_sic}시치"
-        
+  
+        # 메시지 저장
         if reply_message:
             SentMessage.objects.create(
-                room=room,
+                room=reply_room,
                 reply_to=received_message,
                 message=reply_message
             )
         
-        return Response({"message": reply_message})
+        return Response({
+            "room": reply_room,
+            "message": reply_message
+        })
